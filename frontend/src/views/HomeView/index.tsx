1c133b844b61c5b75f76d64c1cd8e9a9951356d3
Sergio Villarruel <sergiovillarruelvasquez@gmail.com>
2022-07-01 19:18:34 -0500

    ✨ add home view listing

--- frontend/src/views/HomeView/index.tsx
+++ frontend/src/views/HomeView/index.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
-import AppCard from 'components/general/AppCard';
-
+import BlockCard from 'components/general/BlockCard';
+import TransactionCard from 'components/general/TransactionCard';
+import React, { useEffect, useState } from 'react';
+import { doTheMine, getChain, getPending } from 'utils/fetchAPI';
+import { IBlockchain } from 'utils/IBlockchain';
+import { ITransaction } from 'utils/ITransaction';
 import './homeView.scss';
 
-const HomeView = () => (
-  <>
-    <div className="home__title">TRANSACTION LIST</div>
-    <div className="home-container">TEST</div>
-  </>
-);
+const HomeView = () => {
+  const [blockchain, setBlockchain] = useState<IBlockchain | null>(null);
+  const [pending, setPending] = useState<ITransaction[] | null>([]);
+  const [mined, setMined] = useState<boolean>(false);
+  const fetchBlockchain = async () => {
+    let response = await getChain();
+    setBlockchain(response.data);
+  };
+
+  const fetchPending = async () => {
+    let response = await getPending();
+    setPending(response.data);
+  };
+
+  const fetchMine = async () => {
+    let response = await doTheMine();
+    setMined(true);
+  };
+
+  useEffect(() => {
+    fetchBlockchain();
+    fetchPending();
+  }, [mined]);
+
+  return (
+    <>
+      <div className="home__title">BLOCKCHAIN LIST</div>
+      <div className="home-container">
+        {blockchain?.chain.map((block) => (
+          <BlockCard {...block} />
+        ))}
+      </div>
+      <div className="home__title">PENDING TRANSACTIONS</div>
+      <div className="home-container">
+        <div className="pending-container">
+          {pending?.length !== 0
+            ? pending?.map((transaction) => (
+                <TransactionCard {...transaction} />
+              ))
+            : 'NO PENDING TRANSACTIONS'}
+        </div>
+        <div>
+          <button type="button" onClick={fetchMine} className="button-mine">
+            MINE!!!
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomeView;
